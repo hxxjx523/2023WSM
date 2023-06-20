530b35ac0e61c769f6b8e743782dc3bc12bce920
heeju <hhj05230@gmail.com>
2023-06-20 10:59:04 +0900

    nav js, style.css -> nav.css

--- 20230609_nav/js/nav.js
+++ 20230609_nav/js/nav.js
@@ -1,22 +1,27 @@
-function sayHello(){
-    alert("안녕");
-}
+// function sayHello(){
+//     alert("안녕");
+// }
 
 // sayHello();
 
-// 1.html -> js
-// 햄버거메뉴 -> navToggleDiv
-// 햄버거메뉴 i -> navToggleI
-// .nav-list -> navListUl
 // const navToggleDiv = document.getElementById("nav_toggle");
-const navToggleI = nav_toggle.getElementByTagName("i")[0];
+const navToggleI = nav_toggle.getElementsByTagName("i")[0];
 const navListUl = document.getElementsByClassName("nav-list")[0];
 
-// 2. Event Handling    
-// 햄버거메뉴 클릭하면 헴버거메뉴 i "bi bi-list" <-> x "bi bi-x-lg"
-// 햄버거메뉴 클릭하면 ,nav-list에 show-menu 보여주기 <-> 안 보여주기
-nav_toggle.onclick = () => alert("안녕"); //ok
-//nav_toggle.onclick = alert("안녕");       //not ok 시험에 나옴
+//2. Event Handling
+// nav_toggle.onclick = () => alert("안녕");
+// nav_toggle.onclick = alert("안녕");
+nav_toggle.onclick = () => {
+    //햄버거메뉴 클릭하면, 햄버거메뉴 i "bi bi-list"<-> X "bi bi-x-lg"
+    navToggleI.classList.toggle("bi-list");
+    navToggleI.classList.toggle("bi-x-lg");
+
+    //햄버거메뉴 클릭하면, .nav-list에 .show-munu보여주기 <-> 안보여주기
+    navListUl.classList.toggle("show-menu");
+
+}
+
+
  
 //----------------------------------------------------
 //html -> js
